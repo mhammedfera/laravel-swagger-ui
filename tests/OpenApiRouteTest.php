d7f3c517a46167bc9cfe47098cfd2049e8ee7769
Blake Bengtson <blake@bengtson.us>
2024-11-27 16:02:02 -0600

    Fixed bug - server_url should work now Wasn't taking into account the 'files' array in the config. server_url was being set in the root, which didn't work. Updated tests as well.

--- tests/OpenApiRouteTest.php
+++ tests/OpenApiRouteTest.php
@@ -39,6 +39,7 @@ class OpenApiRouteTest extends TestCase
     {
         config()->set('swagger-ui.files.0.versions', ['v1' => $openApiFile]);
         config()->set('swagger-ui.files.0.modify_file', true);
+        config()->set('swagger-ui.files.0.server_url', null);
         config()->set('app.url', 'http://foo.bar');
 
         $this->get('swagger/v1')
@@ -57,7 +58,7 @@ class OpenApiRouteTest extends TestCase
         config()->set('app.url', 'http://foo.bar');
         config()->set('swagger-ui.files.0.versions', ['v1' => $openApiFile]);
         config()->set('swagger-ui.files.0.modify_file', true);
-        config()->set('swagger-ui.server_url', 'http://foo.bar/api');
+        config()->set('swagger-ui.files.0.server_url', 'http://foo.bar/api');
 
         $this->get('swagger/v1')
             ->assertStatus(200)
